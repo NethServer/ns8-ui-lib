e363d800c11a554f2efb760e569192772d106e47
Andrea Leardini <andre8244@gmail.com>
2021-11-26 18:05:47 +0100

    Add loop prop to NsEmptyState

--- src/lib-mixins/lottie.js
+++ src/lib-mixins/lottie.js
@@ -1,6 +1,7 @@
 import GhostLottie from "./lottie/ghost.json";
 import GhostDarkBgLottie from "./lottie/ghost-dark-bg.json";
 import RocketLottie from "./lottie/rocket.json";
+import GearsLottie from "./lottie/gears.json";
 
 export default {
   name: "IconService",
@@ -9,6 +10,7 @@ export default {
       GhostLottie,
       GhostDarkBgLottie,
       RocketLottie,
+      GearsLottie,
     };
   },
 };
