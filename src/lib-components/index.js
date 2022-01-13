ff27e34eaae8be7131c5a063dce5caee1c0acbd3
Andrea Leardini <andre8244@gmail.com>
2022-01-13 11:00:12 +0100

    NsWizard

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -21,6 +21,7 @@ export { default as NsCircleTimer } from "./NsCircleTimer.vue";
 export { default as NsDropdownAction } from "./NsDropdownAction.vue";
 export { default as NsTextInput } from "./NsTextInput.vue";
 export { default as NsBackupCard } from "./NsBackupCard.vue";
+export { default as NsWizard } from "./NsWizard.vue";
 
 // Pictograms
 export { default as ExclamationMark } from "./pictograms/ExclamationMark.vue";
