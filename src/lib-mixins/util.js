865bd06714a6e1629037bf28f98eceef5da2fddd
Andrea Leardini <andre8244@gmail.com>
2022-08-05 10:12:49 +0200

    UtilService: add getSha256() method

--- src/lib-mixins/util.js
+++ src/lib-mixins/util.js
@@ -338,5 +338,24 @@ export default {
       );
       return JSON.parse(jsonPayload);
     },
+    /**
+     * get the SHA256 digest of a string
+     */
+    async getSha256(message) {
+      // encode as UTF-8
+      const msgBuffer = new TextEncoder().encode(message);
+
+      // hash the message
+      const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+
+      // convert ArrayBuffer to Array
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+      // convert bytes to hex string
+      const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
+      return hashHex;
+    },
   },
 };
