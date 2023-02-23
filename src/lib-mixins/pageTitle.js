f9302c5929463fde606a9beda5d98d7f88aeba9f
Andrea Leardini <andre8244@gmail.com>
2023-02-23 16:38:25 +0100

    PageTitleService: fix i18n page title
    Use PRODUCT_NAME as page title if i18n strings are not loaded yet

--- src/lib-mixins/pageTitle.js
+++ src/lib-mixins/pageTitle.js
@@ -9,8 +9,12 @@ function getPageTitle(vm) {
 export default {
   name: "PageTitleService",
   created() {
-    const pageTitle = getPageTitle(this);
+    let pageTitle = getPageTitle(this);
     if (pageTitle) {
+      if (pageTitle.includes(".title")) {
+        // fallback title: i18n has not been loaded yet
+        pageTitle = window.parent.CONFIG.PRODUCT_NAME
+      }
       // window.document would be ok for core, but with window.parent.document we support external apps too (window.parent == window, if it's the root window)
       window.parent.document.title = pageTitle;
     }
